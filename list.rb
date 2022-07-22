ba6c2ed73b196e90dd6c2343202e666e8fd96fc6
HammadSiddique <hammad.siddique01@gmail.com>
2022-07-22 18:37:35 +0500

    Test all? and any? method

--- list.rb
+++ list.rb
@@ -1,27 +1,27 @@
 require_relative './enumerable'
 
 class MyList
-    include MyEnumerable
+  include MyEnumerable
 
-    def data(*elems)
-        @list = elems
-    end
+	def initialize(*elems)
+    @list = elems
+  end
 
-    def each(&args)
-        @list.each(&args)
-    end
+  def each(&block)
+    @list.each(&block)
+  end
 end
 
 new_list = MyList.new(1, 2, 3, 4)
 
 # Test #all?
-(new_list.all? {|e| e < 5})
+p(new_list.all? {|e| e < 5}) 
 #  true
-(new_list.all? {|e| e > 5})
+p(new_list.all? {|e| e > 5} )
 # false
 
 # Test #any?
-(new_list.any? {|e| e == 2})
+p(new_list.any? {|e| e == 2})
 # true
-(new_list.any? {|e| e == 5})
+p(new_list.any? {|e| e == 5})
 # false
